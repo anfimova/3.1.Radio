e341f0344b111f61e310e552f9d7165aacf006a1
a.anfimova <a.anfimova@cardsmobile.ru>
2021-05-20 00:25:52 +0300

    Test done

--- src/main/java/ru/netology/domain/Radio.java
+++ src/main/java/ru/netology/domain/Radio.java
@@ -1,4 +1,56 @@
 package ru.netology.domain;
 
 public class Radio {
+
+    private int currentStation;
+    private int currentVolume;
+
+    public int getCurrentStation() {
+        return currentStation;
+    }
+
+    public void setCurrentStation(int currentStation) {
+        if (currentStation >= 0 & currentStation <= 9) {
+            this.currentStation = currentStation;
+        }
+    }
+
+    public void nextStation() {
+        if (currentStation == 9) {
+            currentStation = 0;
+        } else {
+            currentStation += 1;
+        }
+    }
+
+    public void prevStation() {
+        if (currentStation == 0) {
+            currentStation = 9;
+        } else {
+            currentStation -= 1;
+        }
+    }
+
+    public int getCurrentVolume() {
+        return currentVolume;
+    }
+
+    public void setCurrentVolume(int volume) {
+        if (volume > 0 & volume <= 10) {
+            this.currentVolume = volume;
+        }
+    }
+
+    public void nextVolume() {
+        if (currentVolume != 10) {
+            currentVolume += 1;
+        }
+    }
+
+    public void prevVolume() {
+        if (currentVolume != 0) {
+            currentVolume -= 1;
+        }
+    }
+
 }
--- src/test/java/ru/netology/domain/RadioTest.java
+++ src/test/java/ru/netology/domain/RadioTest.java
@@ -1,7 +1,96 @@
 package ru.netology.domain;
 
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class RadioTest {
+
+    Radio radio = new Radio();
+
+    @ParameterizedTest
+    @CsvSource(value = {
+            "0 подходит | 0",
+            "9 подходит | 9",
+            }, delimiter = '|')
+    public void shouldSetStation(String testName, int station) {
+        radio.setCurrentStation(station);
+        assertEquals(station, radio.getCurrentStation());
+    }
+
+    @ParameterizedTest
+    @CsvSource(value = {
+            "-1 не подходит | -1",
+            "10 не подходит | 10"
+    }, delimiter = '|')
+    public void shouldNotSetStation(String testName, int station) {
+        radio.setCurrentStation(station);
+        assertEquals(0, radio.getCurrentStation());
+    }
+
+    @ParameterizedTest
+    @CsvSource(value = {
+            "Увеличить станцию 0 до 1 | 0 | 1",
+            "Увеличить станцию 9 до 0 | 9 | 0"
+    }, delimiter = '|')
+    public void shouldNextStation(String testName, int currentStation, int nextStation) {
+        radio.setCurrentStation(currentStation);
+        radio.nextStation();
+        assertEquals(nextStation, radio.getCurrentStation());
+    }
+
+    @ParameterizedTest
+    @CsvSource(value = {
+            "Уменьшить станцию 0 до 9 | 0 | 9",
+            "Уменьшить станцию 1 до 0 | 1 | 0"
+    }, delimiter = '|')
+    public void shouldPrevStation(String testName, int currentStation, int prevStation) {
+        radio.setCurrentStation(currentStation);
+        radio.prevStation();
+        assertEquals(prevStation, radio.getCurrentStation());
+    }
+
+    @ParameterizedTest
+    @CsvSource(value = {
+            "0 подходит | 0",
+            "10 подходит | 10",
+    }, delimiter = '|')
+    public void shouldSetVolume(String testName, int volume) {
+        radio.setCurrentVolume(volume);
+        assertEquals(volume, radio.getCurrentVolume());
+    }
+
+    @ParameterizedTest
+    @CsvSource(value = {
+            "-1 не подходит | -1",
+            "11 не подходит | 11"
+    }, delimiter = '|')
+    public void shouldNotSetVolume(String testName, int volume) {
+        radio.setCurrentVolume(volume);
+        assertEquals(0, radio.getCurrentVolume());
+    }
+
+    @ParameterizedTest
+    @CsvSource(value = {
+            "Увеличить громкость с 0 до 1 | 0 | 1",
+            "Увеличить громкость с 10 до 10 | 10 | 10"
+    }, delimiter = '|')
+    public void shouldNextVolume(String testName, int currentVolume, int nextVolume) {
+        radio.setCurrentVolume(currentVolume);
+        radio.nextVolume();
+        assertEquals(nextVolume, radio.getCurrentVolume());
+    }
+
+    @ParameterizedTest
+    @CsvSource(value = {
+            "Уменьшить громкость с 0 до 0 | 0 | 0",
+            "Уменьшить громкость с 10 до 9 | 10 | 9"
+    }, delimiter = '|')
+    public void shouldPrevVolume(String testName, int currentVolume, int prevVolume) {
+        radio.setCurrentVolume(currentVolume);
+        radio.prevVolume();
+        assertEquals(prevVolume, radio.getCurrentVolume());
+    }
 
 }
